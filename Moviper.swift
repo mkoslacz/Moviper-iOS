78eedf77b1f18972947d90e46991019aba05dea4
Marek Truszkowski <truszkowski.marek91@gmail.com>
2017-03-20 11:14:44 +0100

    [IPC] add: removing by identifier

--- Moviper.swift
+++ Moviper.swift
@@ -6,66 +6,54 @@
 import Foundation
 import RxSwift
 
-
 final class Moviper {
+
+    static let sharedInstance = Moviper()
+    private init() {}
 
     let ipcConcurrentQueue = DispatchQueue(label: "IpcQueue", attributes: .concurrent)
 
-    // Can't init the singleton
-    private init() {
-    }
+    var presenters = [ViperRxPresenter]()
 
-    static let sharedInstance = Moviper()
-
-    private var presenters = Array<BaseRxPresenter<BaseRxInteractor, BaseRxRouting, BaseRxViewController>>()
-
-    func register(presenter: BaseRxPresenter<BaseRxInteractor, BaseRxRouting, BaseRxViewController>) {
+    func register(presenter: ViperRxPresenter) {
+        //W singletonie trzeba dodawać do array w kolejce serial
         presenters.append(presenter)
     }
 
-    func unregister(presenter: BaseRxPresenter<BaseRxInteractor, BaseRxRouting, BaseRxViewController>) {
-        presenters.remove(object: presenter)
+    func unregister(presenter: ViperRxPresenter) {
+        let index =  presenters.index { (containedPresenter) -> Bool in
+            containedPresenter.identifier == presenter.identifier
+        }
+
+        if let index = index {
+            presenters.remove(at: index)
+        }
     }
 
-    func getPresenters<T:ViperRxPresenter>(presenterType: T.Type) -> Observable<T> {
+    func getPresenters<T: ViperRxPresenter>(presenterType: T.Type) -> Observable<T> {
         return Observable.from(presenters)
-                .filter { (presenter: BaseRxPresenter) in
-                    type(of: presenter) == presenterType
-                }
-                .map { presenter in
-                    presenter as! T
-                }
-                .subscribeOn(ConcurrentDispatchQueueScheduler(queue: ipcConcurrentQueue))
+            .filter { (presenter: ViperRxPresenter) in
+                type(of: presenter) == presenterType
+            }
+            .map { presenter in
+                return presenter as! T
+            }
+            .subscribeOn(ConcurrentDispatchQueueScheduler(queue: ipcConcurrentQueue))
     }
 
-    func getPresenterInstance<T:ViperRxPresenter>(presenterType: T.Type, presenterName: String) -> Maybe<T> {
+    func getPresenterInstance<T: ViperRxPresenter>(presenterType: T.Type, presenterName: String) -> Maybe<T> {
         return getPresenters(presenterType: presenterType)
-                .filter { (presenter: ViperRxPresenter) in
-                    presenter.name == presenterName
-                }
-                .asMaybe()
+            .filter { (presenter: ViperRxPresenter) in
+                presenter.name == presenterName
+            }
+            .asMaybe()
     }
 
     func getPresenterInstanceOrError<T:ViperRxPresenter>(presenterType: T.Type, presenterName: String) -> Single<T> {
         return getPresenters(presenterType: presenterType)
-                .filter { (presenter: ViperRxPresenter) in
-                    presenter.name == presenterName
-                }
-                .asSingle()
+            .filter { (presenter: ViperRxPresenter) in
+                presenter.name == presenterName
+            }
+            .asSingle()
     }
-
-
 }
-
-extension Array where Element: Equatable {
-
-    // Remove first collection element that is equal to the given `object`:
-    mutating func remove(object: Element) {
-        if let index = index(of: object) {
-            remove(at: index)
-        }
-    }
-
-}
-
-
