985924fc3ebf8f5c4a1df8a5f5edf884934f9823
Grzegorz Surma <grzegorz.surma@grupawp.pl>
2017-04-25 09:56:01 +0200

    [Ads] add: base classes

---
+++ BasePresenterForAds.swift
@@ -0,0 +1,9 @@
+//
+//  BasePresenterForAds.swift
+//  videostar
+//
+//  Created by Grzegorz Surma on 25/04/2017.
+//  Copyright © 2017 Grupa Wirtualna Polska. All rights reserved.
+//
+
+import Foundation
---
+++ InteractorForAds.swift
@@ -0,0 +1,9 @@
+//
+//  InteractorForAds.swift
+//  videostar
+//
+//  Created by Grzegorz Surma on 25/04/2017.
+//  Copyright © 2017 Grupa Wirtualna Polska. All rights reserved.
+//
+
+import Foundation
