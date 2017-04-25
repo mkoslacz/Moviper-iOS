e98d91a0478f9761308c816b027af395f2d13be5
Grzegorz Surma <grzegorz.surma@grupawp.pl>
2017-04-25 11:02:14 +0200

    [VIPER] mod: ads moved to project repo

--- DummyRouting.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DummyRouting.swift
-//  videostar
-//
-//  Created by Grzegorz Surma on 25/04/2017.
-//  Copyright © 2017 Grupa Wirtualna Polska. All rights reserved.
-//
-
-import Foundation
--- InteractorForAds.swift
+++
@@ -1,9 +0,0 @@
-//
-//  InteractorForAds.swift
-//  videostar
-//
-//  Created by Grzegorz Surma on 25/04/2017.
-//  Copyright © 2017 Grupa Wirtualna Polska. All rights reserved.
-//
-
-import Foundation
